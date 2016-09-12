7e9b4393e0aa8fe5599c2c18bd46c47fb60afd7d
adri326 <adrien.burgun@orange.fr>
2016-09-12 10:49:03 +0200

    Update parallax.js

--- serenity-strife/parallax.js
+++ serenity-strife/parallax.js
@@ -4,5 +4,9 @@ window.addEventListener("scroll", function(event) {
   var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
   var scrollraw = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
   var scroll = scrollraw/(height-html.clientHeight);
-  document.getElementById("bg").style.filter = "blur("+Math.min(scrollraw, 400)/200*6+"px)"
+  document.getElementById("bg").style.filter = "blur("+Math.min(scrollraw, 400)/200*6+"px)";
+  var elems = document.getElementsByClassName("container");
+  for (n=0; n<elems.length; n++) {
+    elems[n].style.opacity = (300-Math.min(scrollraw, 300))*1
+  }
 });
