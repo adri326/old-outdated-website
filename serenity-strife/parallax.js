4ce6f9ec34486feec62a7103ef866979a64b2ac6
adri326 <adrien.burgun@orange.fr>
2016-09-12 21:57:47 +0200

    Added a small on-scroll zoom

--- serenity-strife/parallax.js
+++ serenity-strife/parallax.js
@@ -5,7 +5,9 @@ function onscroll(event) {
   var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
   var scrollraw = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
   var scroll = scrollraw/(height-html.clientHeight);
-  document.getElementById("bg").style.filter = "blur("+Math.min(scrollraw, 400)/200*6+"px)";
+  var bg = document.getElementById("bg");
+  bg.style.filter = "blur("+Math.min(scrollraw, 400)/200*6+"px)";
+  bg.style.transform = "scale("+(Math.min(scrollraw, 400)/6400+1)+")";
   var elems = document.getElementsByClassName("container");
   for (n=0; n<elems.length; n++) {
     elems[n].style.opacity = (Math.min(scrollraw, 300))/300;
