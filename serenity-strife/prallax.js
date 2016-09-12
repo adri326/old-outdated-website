eaa9cee6cdc1e1d02d401cc28ed5f3c8e3d826b1
adri326 <adrien.burgun@orange.fr>
2016-09-12 10:41:44 +0200

    Update prallax.js

--- serenity-strife/prallax.js
+++ serenity-strife/prallax.js
@@ -2,5 +2,7 @@ window.addEventListener("scroll", function(event) {
   var body = document.body;
   var html = document.documentElement;
   var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
-  var scroll = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+  var scrollraw = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+  var scroll = scrollraw/(height-html.clientHeight);
+  document.getElementById("bg").style.filter = "blur("+scroll*12+"px)"
 });
