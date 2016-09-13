5d6f00f027775471c32aedd021112dcd74d97dde
adri326 <adrien.burgun@orange.fr>
2016-09-13 16:10:41 +0200

    Update parallax.js

--- serenity-strife/parallax.js
+++ serenity-strife/parallax.js
@@ -6,7 +6,8 @@ function onscroll(event) {
   var scrollraw = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
   var scroll = scrollraw/(height-html.clientHeight);
   var bg = document.getElementById("bg");
-  bg.style.filter = "blur("+Math.min(scrollraw, 400)/200*6+"px)";
+  
+  var modifier = 1;
   if (scrollraw<10) {
     bg.style.width = "100%";
     bg.style.height = "100%";
@@ -15,15 +16,19 @@ function onscroll(event) {
     bg.style.width = "50%";
     bg.style.height = "50%";
     bg.style.transform = "scale(2)";
+    modifier = 2;
   } else if (scrollraw<100) {
     bg.style.width = "25%";
     bg.style.height = "25%";
     bg.style.transform = "scale(4)";
+    modifier = 4;
   } else {
     bg.style.width = "10%";
     bg.style.height = "10%";
     bg.style.transform = "scale(10)";
+    modifier = 10;
   }
+  bg.style.filter = "blur("+Math.min(scrollraw, 400)/200*6/modifier+"px)";
   var elems = document.getElementsByClassName("container");
   for (n=0; n<elems.length; n++) {
     elems[n].style.opacity = (Math.min(scrollraw, 300))/300;
